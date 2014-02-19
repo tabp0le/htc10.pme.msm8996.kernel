1b2250d34dbe5560a464cd1de70eb822d3d75084
Joe Swantek <w98568@motorola.com>
2014-02-19 16:41:41 -0600

    msm: lpm-levels: add suspend_again handler
    Call quickwakeup clients to enable us to perform very small amounts of
    processing and then to drop quickly back into suspend.
    
    Propagation of (CR)
    
    Signed-off-by: Jared Suttles <jsuttles@motorola.com>
    Signed-off-by: Joe Swantek <w98568@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Patrick Auchter <auchter@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>
    Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -27,6 +27,7 @@
 #include <linux/console.h>
 #include <linux/suspend.h>
 #include <linux/pm_qos.h>
+#include <linux/quickwakeup.h>
 #include <linux/of_platform.h>
 #include <linux/smp.h>
 #include <linux/remote_spinlock.h>
@@ -1547,6 +1548,7 @@ static const struct platform_suspend_ops lpm_suspend_ops = {
 	.valid = suspend_valid_only_mem,
 	.prepare_late = lpm_suspend_prepare,
 	.wake = lpm_suspend_wake,
+	.suspend_again = quickwakeup_suspend_again,
 };
 
 static void lpm_clk_init(struct platform_device *pdev)
