2c3075b67280931a630871e4238b3c069b2946db
flar2 <asegaert@gmail.com>
2015-05-18 00:23:09 -0400

    sched: don't let pnpmgr drop budget below MIN_BUDGET
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -33,6 +33,7 @@
 
 #include "sched.h"
 #include <trace/events/sched.h>
+#define MIN_BUDGET	40
 
 /*
  * Targeted preemption latency for CPU-bound tasks:
@@ -2530,7 +2531,10 @@ int sched_set_cpu_budget(int cpu, int budget)
 {
 	struct rq *rq = cpu_rq(cpu);
 
-	rq->budget = budget;
+	if (cpu < 6)
+		rq->budget = max(budget, MIN_BUDGET);
+	else
+		rq->budget = budget;
 
 	return 0;
 }
