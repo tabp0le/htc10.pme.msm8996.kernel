dad17889e98c9ea3fa03b30e8383619589d0f078
andip71 <andreasp@gmx.de>
2016-01-29 14:39:55 +0100

    cpufreq: add ZZMoove big.LITTLE Edition (bLE) development version as of 09.01.2016
    all credits to ZaneZam
    
    working features in this first version:
    
    - zzmoove freq scaling method (without limit functionality)
    - proportional scaling (all modes)
    - fast scaling up/down with afs (fully functional)
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -545,7 +545,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIGHTNING)
 extern struct cpufreq_governor cpufreq_gov_lightning;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lightning)
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
