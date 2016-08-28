e81cb9330702f4a82e1bb501bef9bbeecb9fd322
tbalden <illespal@gmail.com>
2016-08-28 15:41:20 -0700

    leds: qpnp rgb: BLN: fix accidental button light off bug
    When screen comes on (navbar blink off case) be sure
    to check blinking is really on. qpnp_rgb_set called when
    backlight comes on anyway, sets blinking to zero, so it
    will solve the issue.
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/leds/leds-qpnp-rgb.c
+++ drivers/leds/leds-qpnp-rgb.c
@@ -3740,7 +3740,9 @@ static int fb_notifier_callback(struct notifier_block *self,
         blank = evdata->data;
         switch (*blank) {
         case FB_BLANK_UNBLANK:
-		qpnp_buttonled_blink(0);
+		if (blinking) {
+			qpnp_buttonled_blink(0);
+		}
 		break;
 	}
     }
