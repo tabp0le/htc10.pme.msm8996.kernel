c76659dd354668a0327794470c8cbac93333fa62
Eliminater74 <eliminater74@gmail.com>
2016-09-28 09:52:41 -0400

    cpufreq: Gov: Introduce Despair Gov
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -197,6 +197,9 @@ struct cs_dbs_tuners {
 	unsigned int sleep_depth;
 	unsigned int boost_enabled;
 	unsigned int boost_count;
+	unsigned int twostep_threshold;
+	unsigned int twostep_counter;
+	u64 twostep_time;
 };
 
 struct ex_dbs_tuners {
