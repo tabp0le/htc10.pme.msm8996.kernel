722964a5d52c9627484f978c49844c910fee9eae
Eliminater74 <eliminater74@gmail.com>
2016-09-28 16:26:22 -0400

    cpufreq: Gov: Introduce Dynamic Interactive Gov * Ported over to 3.10/18 Kernels By @Eliminater74
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    
    cpufreq: Gov: Dynamic_Interactive Quick Fix (oops)
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -531,6 +531,9 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DYNAMIC_INTERACTIVE)
+extern struct cpufreq_governor cpufreq_gov_dynamic_interactive;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_dynamic_interactive)
 #endif
 
 /*********************************************************************
